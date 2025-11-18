a801cebfa07aa12d48604d7b5594787ad5d076a5
unknown <kibisama9@gmail.com>
2025-11-18 04:05:05 -0800

    0.0.1

--- routes/room.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-// const branch = require("../controllers/branch");
-
-// router.get("/", branch.getBranches);
-
-module.exports = router;
